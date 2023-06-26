a4359a37b2c02bd94582a7b0777e91303885a4a7
antoniogisondi <anto.gisondi@tiscali.it>
2023-06-26 12:32:12 +0200

    Inizialize Vite folder and scaffolding

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import axios from 'axios';
+import { store } from './data/store';
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+export default {
+  components: {
+    AppHeader,
+    AppMain
+  }
+
+}
+</script>
+
+<template lang="">
+  <div>
+    <AppHeader/>
+    <AppMain/>
+  </div>
+</template>
+
+<style lang="scss">
+@use './styles/generals.scss' as *;
+@use './styles/partials/variables';
+@use './styles/partials/mixins'
+</style>
